558b00b70cb3a76f1d17bf0cce7b374bc9707e48
Arne Philipeit <arne.philipeit@gmail.com>
2022-08-07 14:39:04 +0200

    Add ir_ext_file_check bazel macro

---
+++ src/lang/processors/ir/ir.bzl
@@ -0,0 +1,15 @@
+load("@rules_cc//cc:defs.bzl", "cc_test")
+load("//src:katara.bzl", "COPTS")
+
+def ir_ext_file_check(name, src, visibility = None):
+    cc_test(
+        name = name,
+        srcs = ["//src/lang/processors/ir:ir_ext_file_check.cc"],
+        copts = COPTS,
+        data = [src],
+        deps = [
+            "//src/ir:ir_lib",
+            "//src/lang/processors/ir",
+            "@gtest//:gtest_main",
+        ],
+    )
---
+++ src/lang/processors/ir/ir_ext_file_check.cc
@@ -0,0 +1,31 @@
+//
+//  ir_file_check.cc
+//  Katara
+//
+//  Created by Arne Philipeit on 8/7/22.
+//  Copyright © 2022 Arne Philipeit. All rights reserved.
+//
+
+#include <filesystem>
+#include <fstream>
+#include <memory>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "src/ir/representation/program.h"
+#include "src/lang/processors/ir/checker/checker.h"
+#include "src/lang/processors/ir/serialization/parse.h"
+
+namespace {
+
+TEST(IrFileTest, ProgramsParseAndAreOkay) {
+  for (const std::filesystem::directory_entry& entry :
+       std::filesystem::recursive_directory_iterator(std::filesystem::current_path())) {
+    if (entry.path().extension() != ".ir") continue;
+    std::ifstream stream(entry.path());
+    std::unique_ptr<ir::Program> program = lang::ir_serialization::ParseProgram(stream);
+    lang::ir_checker::AssertProgramIsOkay(program.get());
+  }
+}
+
+}  // namespace
