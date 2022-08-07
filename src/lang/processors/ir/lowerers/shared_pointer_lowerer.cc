4844ad3c465e90c1f34eab4d3b58e1cbcc25268b
Arne Philipeit <arne.philipeit@gmail.com>
2022-08-07 14:40:19 +0200

    Use IR file instead of C++ code to add shared pointer implementation during lowering

--- src/lang/processors/ir/lowerers/shared_pointer_lowerer.cc
+++ src/lang/processors/ir/lowerers/shared_pointer_lowerer.cc
@@ -8,6 +8,7 @@
 
 #include "shared_pointer_lowerer.h"
 
+#include <fstream>
 #include <memory>
 #include <optional>
 #include <unordered_map>
@@ -15,178 +16,11 @@
 
 #include "src/ir/builder/block_builder.h"
 #include "src/ir/builder/func_builder.h"
+#include "src/lang/processors/ir/serialization/parse.h"
 
 namespace lang {
 namespace ir_lowerers {
 namespace {
-
-constexpr common::Int kControlBlockSize{int64_t{24}};
-constexpr common::Int kWeakRefCountPointerOffset{int64_t{8}};
-constexpr common::Int kDestructorPointerOffset{int64_t{16}};
-
-struct DecomposedShared {
-  std::shared_ptr<ir::Computed> control_block_pointer;
-  std::shared_ptr<ir::Computed> underlying_pointer;
-};
-
-ir::func_num_t BuildMakeSharedFunc(ir::Program* program) {
-  ir_builder::FuncBuilder fb = ir_builder::FuncBuilder::ForNewFuncInProgram(program);
-
-  fb.SetName("make_shared");
-  auto underlying_element_size = fb.AddArg(ir::i64());
-  auto underlying_count = fb.AddArg(ir::i64());
-  auto underlying_destructor = fb.AddArg(ir::func_type());
-  fb.AddResultType(ir::pointer_type());
-  fb.AddResultType(ir::pointer_type());
-
-  ir_builder::BlockBuilder bb = fb.AddEntryBlock();
-
-  auto control_block_size = ir::ToIntConstant(kControlBlockSize);
-  auto underlying_size = bb.IntMul(underlying_element_size, underlying_count);
-  auto total_size = bb.IntAdd(control_block_size, underlying_size);
-  auto control_block_pointer = bb.Malloc(total_size);
-  bb.Store(control_block_pointer, ir::I64One());
-
-  auto weak_ref_count_offset = ir::ToIntConstant(kWeakRefCountPointerOffset);
-  auto weak_ref_count_pointer = bb.OffsetPointer(control_block_pointer, weak_ref_count_offset);
-  bb.Store(weak_ref_count_pointer, ir::I64Zero());
-
-  auto destructor_offset = ir::ToIntConstant(kDestructorPointerOffset);
-  auto destructor_pointer = bb.OffsetPointer(control_block_pointer, destructor_offset);
-  bb.Store(destructor_pointer, underlying_destructor);
-
-  auto underlying_pointer = bb.OffsetPointer(control_block_pointer, control_block_size);
-  bb.Return({control_block_pointer, underlying_pointer});
-
-  return fb.func_number();
-}
-
-ir::func_num_t BuildCopySharedFunc(ir::Program* program, bool copy_is_strong) {
-  ir_builder::FuncBuilder fb = ir_builder::FuncBuilder::ForNewFuncInProgram(program);
-
-  fb.SetName(copy_is_strong ? "strong_copy_shared" : "weak_copy_shared");
-
-  auto control_block_pointer = fb.AddArg(ir::pointer_type());
-  auto old_underlying_pointer = fb.AddArg(ir::pointer_type());
-  auto underlying_pointer_offset = fb.AddArg(ir::i64());
-  fb.AddResultType(ir::pointer_type());
-
-  ir_builder::BlockBuilder bb = fb.AddEntryBlock();
-
-  std::shared_ptr<ir::Computed> ref_count_pointer;
-  if (copy_is_strong) {
-    ref_count_pointer = std::move(control_block_pointer);
-  } else {
-    auto weak_ref_count_offset = ir::ToIntConstant(kWeakRefCountPointerOffset);
-    ref_count_pointer = bb.OffsetPointer(control_block_pointer, weak_ref_count_offset);
-  }
-
-  auto old_ref_count = bb.Load(ir::i64(), ref_count_pointer);
-  auto new_ref_count = bb.IntAdd(old_ref_count, ir::I64One());
-  bb.Store(ref_count_pointer, new_ref_count);
-
-  auto new_underlying_pointer = bb.OffsetPointer(old_underlying_pointer, underlying_pointer_offset);
-  bb.Return({new_underlying_pointer});
-
-  return fb.func_number();
-}
-
-ir::func_num_t BuildDeleteSharedFunc(ir::Program* program, bool pointer_is_strong) {
-  ir_builder::FuncBuilder fb = ir_builder::FuncBuilder::ForNewFuncInProgram(program);
-
-  fb.SetName(pointer_is_strong ? "delete_strong_shared" : "delete_weak_shared");
-
-  auto control_block_pointer = fb.AddArg(ir::pointer_type());
-
-  ir_builder::BlockBuilder entry_bb = fb.AddEntryBlock();
-  ir_builder::BlockBuilder update_count_bb = fb.AddBlock();
-  ir_builder::BlockBuilder count_reaches_zero_bb = fb.AddBlock();
-
-  std::shared_ptr<ir::Computed> ref_count_pointer;
-  if (pointer_is_strong) {
-    ref_count_pointer = control_block_pointer;
-  } else {
-    auto weak_ref_count_offset = ir::ToIntConstant(kWeakRefCountPointerOffset);
-    ref_count_pointer = entry_bb.OffsetPointer(control_block_pointer, weak_ref_count_offset);
-  }
-
-  auto old_ref_count = entry_bb.Load(ir::i64(), ref_count_pointer);
-  auto is_one = entry_bb.IntEq(old_ref_count, ir::I64One());
-  entry_bb.JumpCond(is_one, count_reaches_zero_bb.block_number(), update_count_bb.block_number());
-
-  auto new_ref_count = update_count_bb.IntSub(old_ref_count, ir::I64One());
-  update_count_bb.Store(ref_count_pointer, new_ref_count);
-  update_count_bb.Return();
-
-  auto build_check_other_ref_count = [&fb, control_block_pointer](
-                                         ir_builder::BlockBuilder& check_other_ref_count_bb,
-                                         std::shared_ptr<ir::Computed> other_ref_count) {
-    ir_builder::BlockBuilder keep_heap_bb = fb.AddBlock();
-    ir_builder::BlockBuilder free_heap_bb = fb.AddBlock();
-
-    auto is_zero = check_other_ref_count_bb.IntEq(other_ref_count, ir::I64Zero());
-    check_other_ref_count_bb.JumpCond(is_zero, free_heap_bb.block_number(),
-                                      keep_heap_bb.block_number());
-
-    keep_heap_bb.Return();
-
-    free_heap_bb.Free(control_block_pointer);
-    free_heap_bb.Return();
-  };
-
-  if (pointer_is_strong) {
-    ir_builder::BlockBuilder destruct_underlying_bb = fb.AddBlock();
-    ir_builder::BlockBuilder check_weak_ref_count_bb = fb.AddBlock();
-
-    auto destructor_offset = ir::ToIntConstant(kDestructorPointerOffset);
-    auto destructor_pointer =
-        count_reaches_zero_bb.OffsetPointer(control_block_pointer, destructor_offset);
-    auto destructor = count_reaches_zero_bb.Load(ir::func_type(), destructor_pointer);
-    auto has_no_destructor = count_reaches_zero_bb.IsNil(destructor);
-    count_reaches_zero_bb.JumpCond(has_no_destructor, check_weak_ref_count_bb.block_number(),
-                                   destruct_underlying_bb.block_number());
-
-    auto control_block_size = ir::ToIntConstant(kControlBlockSize);
-    auto underlying_pointer =
-        destruct_underlying_bb.OffsetPointer(control_block_pointer, control_block_size);
-    destruct_underlying_bb.Call(destructor, {}, {underlying_pointer});
-    destruct_underlying_bb.Jump(check_weak_ref_count_bb.block_number());
-
-    auto weak_ref_count_offset = ir::ToIntConstant(kWeakRefCountPointerOffset);
-    auto weak_ref_count_pointer =
-        check_weak_ref_count_bb.OffsetPointer(control_block_pointer, weak_ref_count_offset);
-    auto weak_ref_count = check_weak_ref_count_bb.Load(ir::i64(), weak_ref_count_pointer);
-    build_check_other_ref_count(check_weak_ref_count_bb, weak_ref_count);
-
-  } else {
-    auto strong_ref_count = count_reaches_zero_bb.Load(ir::i64(), control_block_pointer);
-    build_check_other_ref_count(count_reaches_zero_bb, strong_ref_count);
-  }
-
-  return fb.func_number();
-}
-
-ir::func_num_t BuildValidateWeakSharedFunc(ir::Program* program) {
-  ir_builder::FuncBuilder fb = ir_builder::FuncBuilder::ForNewFuncInProgram(program);
-
-  fb.SetName("validate_weak_shared");
-  auto control_block_pointer = fb.AddArg(ir::pointer_type());
-
-  ir_builder::BlockBuilder entry_bb = fb.AddEntryBlock();
-  ir_builder::BlockBuilder ok_bb = fb.AddBlock();
-  ir_builder::BlockBuilder panic_bb = fb.AddBlock();
-
-  auto strong_ref_count = entry_bb.Load(ir::i64(), control_block_pointer);
-  auto is_zero = entry_bb.IntEq(strong_ref_count, ir::I64Zero());
-  entry_bb.JumpCond(is_zero, panic_bb.block_number(), ok_bb.block_number());
-
-  ok_bb.Return();
-
-  panic_bb.AddInstr<ir_ext::PanicInstr>(
-      std::make_shared<ir_ext::StringConstant>("attempted to access deleted weak pointer"));
-
-  return fb.func_number();
-}
 
 struct LoweringFuncs {
   ir::func_num_t make_shared_func_num;
@@ -195,6 +29,25 @@ struct LoweringFuncs {
   ir::func_num_t delete_strong_shared_func_num;
   ir::func_num_t delete_weak_shared_func_num;
   ir::func_num_t validate_weak_shared_func_num;
+};
+
+LoweringFuncs AddLoweringFuncsToProgram(ir::Program* program) {
+  std::ifstream stream("src/lang/processors/ir/lowerers/shared_pointer_impl.ir");
+  std::vector<ir::Func*> funcs =
+      lang::ir_serialization::ParseAdditionalFuncsForProgram(program, stream);
+  return LoweringFuncs{
+      .make_shared_func_num = funcs.at(0)->number(),
+      .strong_copy_shared_func_num = funcs.at(1)->number(),
+      .weak_copy_shared_func_num = funcs.at(2)->number(),
+      .delete_strong_shared_func_num = funcs.at(3)->number(),
+      .delete_weak_shared_func_num = funcs.at(4)->number(),
+      .validate_weak_shared_func_num = funcs.at(5)->number(),
+  };
+}
+
+struct DecomposedShared {
+  std::shared_ptr<ir::Computed> control_block_pointer;
+  std::shared_ptr<ir::Computed> underlying_pointer;
 };
 
 void LowerSharedPointerArgsOfFunc(
@@ -614,14 +467,7 @@ void LowerSharedPointersInFunc(const LoweringFuncs& lowering_funcs, ir::Func* fu
 }  // namespace
 
 void LowerSharedPointersInProgram(ir::Program* program) {
-  LoweringFuncs lowering_funcs{
-      .make_shared_func_num = BuildMakeSharedFunc(program),
-      .strong_copy_shared_func_num = BuildCopySharedFunc(program, /*copy_is_strong=*/true),
-      .weak_copy_shared_func_num = BuildCopySharedFunc(program, /*copy_is_strong=*/false),
-      .delete_strong_shared_func_num = BuildDeleteSharedFunc(program, /*pointer_is_strong=*/true),
-      .delete_weak_shared_func_num = BuildDeleteSharedFunc(program, /*pointer_is_strong=*/false),
-      .validate_weak_shared_func_num = BuildValidateWeakSharedFunc(program),
-  };
+  LoweringFuncs lowering_funcs = AddLoweringFuncsToProgram(program);
 
   for (auto& func : program->funcs()) {
     LowerSharedPointersInFunc(lowering_funcs, func.get());
