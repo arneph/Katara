0d3c50cb1e1eb702745df1298c9babe0aadaaf5c
Arne Philipeit <arne.philipeit@gmail.com>
2022-12-03 17:35:37 +0100

    Add IR scanner fuzz test

---
+++ src/ir/serialization/scanner_fuzz_test.cc
@@ -0,0 +1,31 @@
+//
+//  scanner_fuzz_test.cc
+//  Katara
+//
+//  Created by Arne Philipeit on 12/19/20.
+//  Copyright © 2020 Arne Philipeit. All rights reserved.
+//
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+#include "src/common/positions/positions.h"
+#include "src/ir/issues/issues.h"
+#include "src/ir/serialization/scanner.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::string contents(reinterpret_cast<const char*>(data), size);
+
+  common::PosFileSet file_set;
+  common::PosFile* file = file_set.AddFile("test_file.ir", contents);
+  ir_issues::IssueTracker issue_tracker(&file_set);
+
+  ir_serialization::Scanner scanner(file, issue_tracker);
+
+  while (scanner.token() != ir_serialization::Scanner::kEoF) {
+    scanner.Next();
+  }
+
+  return 0;
+}
