60e5306cd81777dd70a73954d1bd2e434f774e89
Arne Philipeit <arne.philipeit@gmail.com>
2022-12-04 16:08:20 +0100

    Add lang IR extension parser fuzz test

---
+++ src/lang/processors/ir/serialization/parse_fuzz_test.cc
@@ -0,0 +1,28 @@
+//
+//  parse_fuzz_test.cc
+//  Katara
+//
+//  Created by Arne Philipeit on 12/19/20.
+//  Copyright © 2020 Arne Philipeit. All rights reserved.
+//
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <string>
+
+#include "src/common/positions/positions.h"
+#include "src/ir/issues/issues.h"
+#include "src/ir/representation/program.h"
+#include "src/lang/processors/ir/serialization/parse.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::string contents(reinterpret_cast<const char*>(data), size);
+
+  common::PosFileSet file_set;
+  common::PosFile* file = file_set.AddFile("test_file.ir", contents);
+  ir_issues::IssueTracker issue_tracker(&file_set);
+  std::unique_ptr<ir::Program> program = lang::ir_serialization::ParseProgram(file, issue_tracker);
+
+  return 0;
+}
