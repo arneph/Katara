553f88313387dfc9771d9dd66044dec262f7bf38
Arne Philipeit <arne.philipeit@gmail.com>
2023-11-12 10:07:44 -0500

    Add func arguments to FuncValues

--- src/ir/analyzers/func_values_builder.cc
+++ src/ir/analyzers/func_values_builder.cc
@@ -16,24 +16,41 @@
 
 namespace ir_analyzers {
 
+using ir_info::FuncValues;
+
+namespace {
+
+void AddFuncArguments(const ir::Func* func, FuncValues& func_values) {
+  for (const std::shared_ptr<ir::Computed>& arg : func->args()) {
+    func_values.AddValue(arg.get());
+  }
+}
+
+void AddInstrValues(ir::Instr* instr, FuncValues& func_values) {
+  for (std::shared_ptr<ir::Computed> defined_value : instr->DefinedValues()) {
+    func_values.AddValue(defined_value.get());
+    func_values.SetInstrDefiningValue(instr, defined_value.get());
+  }
+  for (std::shared_ptr<ir::Value> used_value : instr->UsedValues()) {
+    if (used_value->kind() == ir::Value::Kind::kInherited) {
+      used_value = std::static_pointer_cast<ir::InheritedValue>(used_value)->value();
+    }
+    if (used_value->kind() != ir::Value::Kind::kComputed) {
+      continue;
+    }
+    auto used_computed = static_cast<ir::Computed*>(used_value.get());
+    func_values.AddInstrUsingValue(instr, used_computed);
+  }
+}
+
+}  // namespace
+
 const ir_info::FuncValues FindValuesInFunc(const ir::Func* func) {
   ir_info::FuncValues func_values;
+  AddFuncArguments(func, func_values);
   for (const std::unique_ptr<ir::Block>& block : func->blocks()) {
     for (const std::unique_ptr<ir::Instr>& instr : block->instrs()) {
-      for (std::shared_ptr<ir::Computed> defined_value : instr->DefinedValues()) {
-        func_values.AddValue(defined_value.get());
-        func_values.SetInstrDefiningValue(instr.get(), defined_value.get());
-      }
-      for (std::shared_ptr<ir::Value> used_value : instr->UsedValues()) {
-        if (used_value->kind() == ir::Value::Kind::kInherited) {
-          used_value = std::static_pointer_cast<ir::InheritedValue>(used_value)->value();
-        }
-        if (used_value->kind() != ir::Value::Kind::kComputed) {
-          continue;
-        }
-        auto used_computed = static_cast<ir::Computed*>(used_value.get());
-        func_values.AddInstrUsingValue(instr.get(), used_computed);
-      }
+      AddInstrValues(instr.get(), func_values);
     }
   }
   return func_values;
--- src/ir/analyzers/func_values_builder_test.cc
+++ src/ir/analyzers/func_values_builder_test.cc
@@ -25,6 +25,7 @@ namespace {
 
 using ::ir_info::FuncValues;
 using ::testing::IsEmpty;
+using ::testing::UnorderedElementsAre;
 
 TEST(FindValuesInFuncTest, HandlesEmptyFunc) {
   std::unique_ptr<ir::Program> input_program = ir_serialization::ParseProgramOrDie(R"ir(
@@ -48,13 +49,164 @@ TEST(FindValuesInFuncTest, HandlesEmptyFunc) {
   EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kPointer), IsEmpty());
   EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kFunc), IsEmpty());
 
-  EXPECT_EQ(func_values.GetInstrDefiningValue(0), nullptr);
-  EXPECT_EQ(func_values.GetInstrDefiningValue(1), nullptr);
-  EXPECT_EQ(func_values.GetInstrDefiningValue(123), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(0)), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(1)), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(123)), nullptr);
 
-  EXPECT_THAT(func_values.GetInstrsUsingValue(0), IsEmpty());
-  EXPECT_THAT(func_values.GetInstrsUsingValue(1), IsEmpty());
-  EXPECT_THAT(func_values.GetInstrsUsingValue(123), IsEmpty());
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(0)), IsEmpty());
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(1)), IsEmpty());
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(123)), IsEmpty());
+}
+
+TEST(FindValuesInFuncTest, HandlesSingleComputation) {
+  std::unique_ptr<ir::Program> input_program = ir_serialization::ParseProgramOrDie(R"ir(
+@0 f() => (i64) {
+{0}
+  %42:i64 = ineg #1234
+  ret %42
+}
+)ir");
+  ir::Func* func = input_program->GetFunc(ir::func_num_t(0));
+  ir::Block* block = func->entry_block();
+  ir::Instr* ineg_instr = block->instrs().at(0).get();
+  ir::Instr* ret_instr = block->instrs().at(1).get();
+
+  const FuncValues func_values = FindValuesInFunc(func);
+
+  EXPECT_THAT(func_values.GetValues(), UnorderedElementsAre(ir::value_num_t(42)));
+
+  EXPECT_THAT(func_values.GetValuesWithType(ir::bool_type()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::u8()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::i64()), UnorderedElementsAre(ir::value_num_t(42)));
+  EXPECT_THAT(func_values.GetValuesWithType(ir::pointer_type()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::func_type()), IsEmpty());
+
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kBool), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kInt),
+              UnorderedElementsAre(ir::value_num_t(42)));
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kPointer), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kFunc), IsEmpty());
+
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(0)), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(1)), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(42)), ineg_instr);
+
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(0)), IsEmpty());
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(1)), IsEmpty());
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(42)),
+              UnorderedElementsAre(ret_instr));
+}
+
+TEST(FindValuesInFuncTest, HandlesFuncArgument) {
+  std::unique_ptr<ir::Program> input_program = ir_serialization::ParseProgramOrDie(R"ir(
+@0 f(%41:i64) => (i64) {
+{0}
+  %42:i64 = ineg %41
+  ret %42
+}
+)ir");
+  ir::Func* func = input_program->GetFunc(ir::func_num_t(0));
+  ir::Block* block = func->entry_block();
+  ir::Instr* ineg_instr = block->instrs().at(0).get();
+  ir::Instr* ret_instr = block->instrs().at(1).get();
+
+  const FuncValues func_values = FindValuesInFunc(func);
+
+  EXPECT_THAT(func_values.GetValues(),
+              UnorderedElementsAre(ir::value_num_t(41), ir::value_num_t(42)));
+
+  EXPECT_THAT(func_values.GetValuesWithType(ir::bool_type()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::u8()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::i64()),
+              UnorderedElementsAre(ir::value_num_t(41), ir::value_num_t(42)));
+  EXPECT_THAT(func_values.GetValuesWithType(ir::pointer_type()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::func_type()), IsEmpty());
+
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kBool), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kInt),
+              UnorderedElementsAre(ir::value_num_t(41), ir::value_num_t(42)));
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kPointer), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kFunc), IsEmpty());
+
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(41)), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(42)), ineg_instr);
+
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(41)),
+              UnorderedElementsAre(ineg_instr));
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(42)),
+              UnorderedElementsAre(ret_instr));
+}
+
+TEST(FindValuesInFuncTest, HandlesMultipleBlocks) {
+  std::unique_ptr<ir::Program> input_program = ir_serialization::ParseProgramOrDie(R"ir(
+@0 f(%40:b, %41:i64) => (i64, b) {
+{0}
+  jcc %40, {1}, {2}
+{1}
+  %42:i64 = ineg %41
+  jmp {3}
+{2}
+  %43:i64 = inot %41
+  jmp {3}
+{3}
+  %44:i64 = phi %42:{1}, %43:{2}
+  ret %44, %40
+}
+)ir");
+
+  ir::Func* func = input_program->GetFunc(ir::func_num_t(0));
+  ir::Block* block0 = func->GetBlock(ir::block_num_t(0));
+  ir::Instr* jcc_instr = block0->instrs().at(0).get();
+
+  ir::Block* block1 = func->GetBlock(ir::block_num_t(1));
+  ir::Instr* ineg_instr = block1->instrs().at(0).get();
+
+  ir::Block* block2 = func->GetBlock(ir::block_num_t(2));
+  ir::Instr* inot_instr = block2->instrs().at(0).get();
+
+  ir::Block* block3 = func->GetBlock(ir::block_num_t(3));
+  ir::Instr* phi_instr = block3->instrs().at(0).get();
+  ir::Instr* ret_instr = block3->instrs().at(1).get();
+
+  const FuncValues func_values = FindValuesInFunc(func);
+
+  EXPECT_THAT(func_values.GetValues(),
+              UnorderedElementsAre(ir::value_num_t(40), ir::value_num_t(41), ir::value_num_t(42),
+                                   ir::value_num_t(43), ir::value_num_t(44)));
+
+  EXPECT_THAT(func_values.GetValuesWithType(ir::bool_type()),
+              UnorderedElementsAre(ir::value_num_t(40)));
+  EXPECT_THAT(func_values.GetValuesWithType(ir::u8()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::i64()),
+              UnorderedElementsAre(ir::value_num_t(41), ir::value_num_t(42), ir::value_num_t(43),
+                                   ir::value_num_t(44)));
+  EXPECT_THAT(func_values.GetValuesWithType(ir::pointer_type()), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithType(ir::func_type()), IsEmpty());
+
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kBool),
+              UnorderedElementsAre(ir::value_num_t(40)));
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kInt),
+              UnorderedElementsAre(ir::value_num_t(41), ir::value_num_t(42), ir::value_num_t(43),
+                                   ir::value_num_t(44)));
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kPointer), IsEmpty());
+  EXPECT_THAT(func_values.GetValuesWithTypeKind(ir::TypeKind::kFunc), IsEmpty());
+
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(40)), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(41)), nullptr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(42)), ineg_instr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(43)), inot_instr);
+  EXPECT_EQ(func_values.GetInstrDefiningValue(ir::value_num_t(44)), phi_instr);
+
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(40)),
+              UnorderedElementsAre(jcc_instr, ret_instr));
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(41)),
+              UnorderedElementsAre(ineg_instr, inot_instr));
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(42)),
+              UnorderedElementsAre(phi_instr));
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(43)),
+              UnorderedElementsAre(phi_instr));
+  EXPECT_THAT(func_values.GetInstrsUsingValue(ir::value_num_t(44)),
+              UnorderedElementsAre(ret_instr));
 }
 
 }  // namespace
